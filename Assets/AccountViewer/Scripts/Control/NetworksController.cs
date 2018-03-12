2dbc059113bb9d7b1a4eaab95bc42ddfbb9a9597
Jai Baldan <devkirbyrawr@gmail.com>
2018-03-13 00:00:44 +0100

    Rebuilding lost changes, step by step, don't give up..

---
+++ Assets/AccountViewer/Scripts/Control/NetworksController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworksController : MonoBehaviour {
+
+	[System.Serializable]
+	public class Network 
+	{
+		public string name;
+		public string passphrase;
+		public List<Server> servers = new List<Server>();
+
+		public Network(string name, string passphrase) 
+		{
+			this.name = name;
+			this.passphrase = passphrase;
+		}
+	}
+
+	[System.Serializable]
+	public class Server 
+	{
+		public string name;
+		public string url;
+
+		public Server(string name, string url) 
+		{
+			this.name = name;
+			this.url = url;
+		}
+	}
+
+	public List<Network> networks = new List<Network>();
+
+	public void AddNetwork(Network network) 
+	{
+		networks.Add(network);
+	}
+
+	public void AddServer(Network network, Server server) 
+	{
+		network.servers.Add(server);
+	}
+
+	public void ChangeNetwork(Network network, Server server) 
+	{
+
+	}
+}
---
+++ Assets/AccountViewer/Scripts/UI/General/UIInputModule.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AccountViewer.UI;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class UIInputModule : UIModule
+{
+    public enum Mode { Add, Edit }
+
+    public abstract string inputName { get; }
+    public CanvasGroup canvasGroup;
+    public Text titleLabel;
+    public Text okLabel;
+
+    protected void Show(Mode mode)
+    {
+        //Transparency
+        ShowTransparency();
+
+        //Canvas
+        canvasGroup.SetInteractable(true);
+        canvasGroup.DOFade(1, animationSpeed);
+
+        switch (mode)
+        {
+            case Mode.Add:
+                titleLabel.text = string.Concat("Add ", inputName);
+                okLabel.text = "Add";
+                break;
+
+            case Mode.Edit:
+                titleLabel.text = string.Concat("Edit ", inputName);
+                okLabel.text = "Edit";
+                break;
+        }
+    }
+
+    protected void Hide()
+    {
+        //Transparency
+        HideTransparency();
+
+        //Canvas
+        canvasGroup.SetInteractable(false);
+        canvasGroup.DOFade(0, animationSpeed);
+    }
+
+	protected abstract void OnClickAdd();
+
+	protected abstract void OnClickEdit();
+
+    protected abstract bool IsDataValid();
+}
